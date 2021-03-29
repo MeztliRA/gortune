1526265b1250f945167703587fe9bae4bbc663e3
MeztliRA <MeztliRA@gmail.com>
2021-03-29 16:00:53 +0700

    Add Quotes array in quote.go

---
+++ quotes/quote.go
@@ -0,0 +1,7 @@
+package quotes
+
+var Quotes = [3]string{
+	"I learn something every time I go into the mountains.",
+	"There is nothing like a dream to create the future.",
+	"The stars don't look bigger, but they do look brighter.",
+}
