cb183fcc40e2e1503e3f74a953c9ec627bec6ad1
MeztliRA <MeztliRA@gmail.com>
2021-04-01 14:49:48 +0700

    Omitted the size of the Quotes array

--- quotes/quote.go
+++ quotes/quote.go
@@ -1,6 +1,6 @@
 package quotes
 
-var Quotes = [3]string{
+var Quotes = []string{
 	"I learn something every time I go into the mountains.",
 	"There is nothing like a dream to create the future.",
 	"The stars don't look bigger, but they do look brighter.",
