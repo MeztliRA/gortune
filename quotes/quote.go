ed25f5779e78027581310872a5cca545af697a28
MeztliRA <MeztliRA@gmail.com>
2021-04-01 14:58:15 +0700

    Add new quotes

--- quotes/quote.go
+++ quotes/quote.go
@@ -4,4 +4,6 @@ var Quotes = []string{
 	"I learn something every time I go into the mountains.",
 	"There is nothing like a dream to create the future.",
 	"The stars don't look bigger, but they do look brighter.",
+	"Don't communicate by sharing memory, share memory by communicating.",
+	"The internet is a great way to get on the net.",
 }
