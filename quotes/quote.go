59613ebea1ae320a8c4138eb0bb348aadc30fbd1
MeztliRA <MeztliRA@gmail.com>
2021-04-02 17:51:37 +0700

    Add new quotes

--- quotes/quote.go
+++ quotes/quote.go
@@ -6,4 +6,7 @@ var Quotes = []string{
 	"The stars don't look bigger, but they do look brighter.",
 	"Don't communicate by sharing memory, share memory by communicating.",
 	"The internet is a great way to get on the net.",
+	"The essence of strategy is choosing what not to do.",
+	"Weak people revenge. Strong people forgive. Intelligent People Ignore.",
+	"A mind is like a parachute. It doesn't work if it is not open.",
 }
